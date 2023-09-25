387576960de3a28707ed84d1df533e658dae9584
Akshat Sharma <akshatcoder@outlook.com>
2023-09-25 23:18:10 +0530

    off chain worker for hpos

--- src/lib.rs
+++ src/lib.rs
@@ -1,71 +1,82 @@
 use reqwest;
+use serde::{Deserialize, Serialize};
 use solana_client::rpc_client::RpcClient;
 use solana_sdk::{
-    pubkey::Pubkey,
-    signature::{Keypair, Signer},
-    transaction::Transaction,
     instruction::Instruction,
+    message::Message,
+    pubkey::Pubkey,
+    signature::{Keypair, read_keypair_file, Signer},
+    transaction::Transaction,
 };
 use std::str::FromStr;
 
-pub async fn fetch_binance_price(symbol: &str) -> Result<u64, reqwest::Error> {
-    let url = format!("https://api.binance.com/api/v3/ticker/price?symbol={}", symbol);
-    let response: serde_json::Value = reqwest::get(&url).await?.json().await?;
-    let price: u64 = response["price"].as_str().unwrap().parse().unwrap();
-    Ok(price)
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AssetContext {
+    mark_price: f64,
+    current_funding: f64,
+    open_interest: f64,
 }
 
-pub fn send_prices_to_solana(btc_price: u64, eth_price: u64) -> Result<(), Box<dyn std::error::Error>> {
-    // Initialize Solana client
-    let rpc_client = RpcClient::new("https://api.mainnet-beta.solana.com");
+pub async fn fetch_hyperliquid_price() -> Result<Vec<AssetContext>, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let res = client
+        .post("https://api.hyperliquid.xyz/info")
+        .header("Content-Type", "application/json")
+        .json(&serde_json::json!({
+            "type": "metaAndAssetCtxs"
+        }))
+        .send()
+        .await?;
 
-    // Solana program ID and price data account
-    let program_id = Pubkey::from_str("EVkRuHAH76x6GYsx4isftKWCseJTCVtQYvh64MEA6q2U")?;
-    let price_data_account = Pubkey::from_str("AStbVxPR31uzdqSaF96cQ9oM1J1UWL31kfA6gYQKjibs")?;
+    let asset_contexts: Vec<AssetContext> = res.json().await?;
+    Ok(asset_contexts)
+}
 
-    // keypair
-    let keypair = Keypair::new();
+pub fn send_prices_to_solana(
+    asset_contexts: Vec<AssetContext>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let rpc_client = RpcClient::new("https://api.devnet.solana.com");
+    let program_id = Pubkey::from_str("6pbB1VzzU5VDtmQBkxmQNAcSbPnS9Vyon6kBb2YwgKeo")?;
+    let price_data_account = Pubkey::from_str("GqnDxrf8ra4WFD9ZL8vWR5bj7zftBZT8ZJC7wB5w11Xs")?;
+    let keypair = read_keypair_file("/Users/akshatsharma/octane/keys/octane.json")?;
 
-    // Create instruction data
     let mut instruction_data = vec![];
-    instruction_data.extend_from_slice(&btc_price.to_le_bytes());
-    instruction_data.extend_from_slice(&eth_price.to_le_bytes());
+    for asset in asset_contexts {
+        instruction_data.extend_from_slice(&asset.mark_price.to_le_bytes());
+    }
 
-    // Create the instructions
-    let instructions: Vec<Instruction> = vec![
-        Instruction {
-            program_id,
-            accounts: vec![
-                solana_sdk::instruction::AccountMeta::new(price_data_account, false),
-            ],
-            data: instruction_data,
-        },
-    ];
+    let instruction = Instruction {
+        program_id,
+        accounts: vec![solana_sdk::instruction::AccountMeta::new(
+            price_data_account,
+            false,
+        )],
+        data: instruction_data,
+    };
 
-// Create the message
-    let message = solana_sdk::message::Message::new_with_payer(
-        &instructions,
-        Some(&keypair.pubkey()),
-    );
+    // let message = Message::new(&[instruction], Some(&keypair.pubkey()));
+    let mut transaction = Transaction::new_with_payer(&[instruction], Some(&keypair.pubkey()));
 
-// Create the transaction
-    let mut transaction = Transaction::new_with_payer(
-        &message,
-        Some(&keypair.pubkey()),
-    );
-    transaction.try_sign(&[&keypair], rpc_client.get_recent_blockhash()?.0)?;
+    let blockhash = rpc_client.get_latest_blockhash()?;
+    transaction.try_sign(&[&keypair], blockhash)?;
 
-// Send the transaction
     let result = rpc_client.send_and_confirm_transaction(&transaction);
 
     match result {
         Ok(_) => {
             println!("Transaction succeeded");
             Ok(())
-        },
+        }
         Err(err) => {
             println!("Transaction failed: {:?}", err);
             Err(Box::new(err))
-        },
+        }
     }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let asset_contexts = fetch_hyperliquid_price().await?;
+    send_prices_to_solana(asset_contexts)?;
+    Ok(())
 }
